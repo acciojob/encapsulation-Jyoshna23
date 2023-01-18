0e5541e3e449485fe68d7fe3445f611b6ebda790
unknown <dammajyoshna123@gmail.com>
2023-01-18 16:25:37 +0530

    Encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+//        obj.name = "jyoshna";
+//        System.out.println(obj.name + "Error: you might seen some error, please not it down in commnets");
+
+        obj.setName("jyoshna");
+        obj.getName();
+    }
 }
