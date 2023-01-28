cd7a01da1ab22f73eabde39bc943097a821de88a
Jyoshna <118041308+Jyoshna23@users.noreply.github.com>
2023-01-28 09:23:14 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,8 +3,7 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         RWOnly obj = new RWOnly();
-//        obj.name = "jyoshna";
-//        System.out.println(obj.name + "Error: you might seen some error, please not it down in commnets");
+
 
         obj.setName("jyoshna");
         obj.getName();
